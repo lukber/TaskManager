4c00cd5683a4146854d3ec3dac0b37cd5a35a1e1
lukber <lukber@seznam.cz>
2013-11-22 19:30:56 +0100

    task list item

--- task-manager-android/src/main/java/cz/czechGeeks/taskManager/client/android/adapter/TaskListAdapter.java
+++ task-manager-android/src/main/java/cz/czechGeeks/taskManager/client/android/adapter/TaskListAdapter.java
@@ -3,23 +3,23 @@ package cz.czechGeeks.taskManager.client.android.adapter;
 import java.io.Serializable;
 import java.util.List;
 
-import android.R;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import cz.czechGeeks.taskManager.client.android.R;
 import cz.czechGeeks.taskManager.client.android.model.TaskModel;
 
-public class TaskListAdapter extends ArrayAdapter<TaskModel> implements Serializable{
+public class TaskListAdapter extends ArrayAdapter<TaskModel> implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private final LayoutInflater mInflater;
 
 	public TaskListAdapter(Context context) {
-		super(context, android.R.layout.simple_list_item_2);
+		super(context, R.layout.task_list_item);
 		mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 	}
 
@@ -35,14 +35,17 @@ public class TaskListAdapter extends ArrayAdapter<TaskModel> implements Serializ
 		View view;
 
 		if (convertView == null) {
-			view = mInflater.inflate(android.R.layout.simple_list_item_2, parent, false);
+			view = mInflater.inflate(R.layout.task_list_item, parent, false);
 		} else {
 			view = convertView;
 		}
 
 		TaskModel item = getItem(position);
-		((TextView) view.findViewById(R.id.text1)).setText(item.getName());
-		((TextView) view.findViewById(R.id.text2)).setText(item.getDesc());
+
+		((TextView) view.findViewById(R.id.taskName)).setText(item.getName());
+		((TextView) view.findViewById(R.id.taskDesc)).setText(item.getDesc());
+		((TextView) view.findViewById(R.id.taskCateg)).setText(item.getCategName());
+		((TextView) view.findViewById(R.id.taskFinishToDate)).setText((item.getFinishToDate() != null) ? java.text.DateFormat.getDateTimeInstance().format(item.getFinishToDate()) : "");
 
 		return view;
 	}
